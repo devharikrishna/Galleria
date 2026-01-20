6b7e17b93ec210b99b66b230ed547ee6e322fcad
TrogonNative <native.trogon@gmail.com>
2026-01-20 21:19:20 +0530

    20012026g

--- app/src/main/java/com/irah/galleria/MainActivity.kt
+++ app/src/main/java/com/irah/galleria/MainActivity.kt
@@ -3,18 +3,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.irah.galleria.ui.MainScreen
-import com.irah.galleria.ui.theme.GalleriaTheme
-import dagger.hilt.android.AndroidEntryPoint
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.irah.galleria.domain.model.AppSettings
+import com.irah.galleria.ui.MainScreen
+import com.irah.galleria.ui.common.PermissionWrapper
 import com.irah.galleria.ui.settings.SettingsViewModel
+import com.irah.galleria.ui.theme.GalleriaTheme
+import dagger.hilt.android.AndroidEntryPoint
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,24 +27,10 @@ class MainActivity : ComponentActivity() {
                 useDynamicColor = settings.useDynamicColor,
                 accentColor = settings.accentColor
             ) {
-                com.irah.galleria.ui.common.PermissionWrapper {
+                PermissionWrapper {
                     MainScreen()
                 }
             }
         }
-    }
-}
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GalleriaTheme {
-        Greeting("Android")
     }
 }
